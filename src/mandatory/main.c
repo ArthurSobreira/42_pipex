8ab05a626cdfe2d584d50cc5f8b82272ec664e5c
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-24 19:47:05 -0300

    Update main.c

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,14 +6,24 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 11:58:26 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/23 12:02:54 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/24 19:41:41 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int main(void)
+int	main(void)
 {
-	ft_printf("Hello World\n");
-	return 0;
+	const char	*file_name;
+
+	file_name = "test.txt";
+	if (access(file_name, F_OK) == 0)
+		printf("The file %s exist\n", file_name);
+	if (access(file_name, R_OK) == 0)
+		printf("The file %s is readable\n", file_name);
+	if (access(file_name, W_OK) == 0)
+		printf("The file %s is writable\n", file_name);
+	if (access(file_name, X_OK) == 0)
+		printf("The file %s is executable\n", file_name);
+	return (0);
 }
