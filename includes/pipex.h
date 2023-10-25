b03429f4c364117608b7cf9317383e3df043df0b
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-25 12:09:38 -0300

    Update pipex header

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 12:01:30 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/24 16:01:20 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/25 12:02:06 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 # include "../libs/libft/libft.h"
 # include <fcntl.h>
 # include <unistd.h>
-
-# include <stdio.h>
+# include <stdlib.h>
 
 #endif
