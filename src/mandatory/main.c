b33c46a4d0025c172ea4f5aa93e3243c7c861546
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-26 10:42:11 -0300

    Update main.c

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,32 +6,56 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 11:58:26 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/25 09:51:46 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/26 10:40:35 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	test_access(void);
-
-int	main(void)
+static void	test_execve(void)
 {
-	// Run 'chmod xxx test.txt && ls -la test.txt && ./pipex'
-	test_access();
-	return (0);
+	char	*argv[] = {"ls", "-lha", NULL};
+	char	*path;
+
+	path = "/bin/ls";
+	if (execve(path, argv, NULL) < 0)
+		ft_printf("Unable to run '%s'\n", path);
+	ft_printf("\n");
+}
+
+static void	test_execve_script(void)
+{
+	char	*argv[] = {"/bin/bash", "script.sh", NULL};
+	char	*path;
+
+	path = "/bin/bash";
+	if (execve(path, argv, NULL) < 0)
+		ft_printf("Unable to run '%s'\n", path);
+	ft_printf("\n");
 }
 
 static void	test_access(void)
 {
 	const char	*file_name;
 
-	file_name = "test.txt";
+	file_name = "script.sh";
 	if (access(file_name, F_OK) == 0)
-		printf("The file %s exist\n", file_name);
+		ft_printf("The file %s exist\n", file_name);
 	if (access(file_name, R_OK) == 0)
-		printf("The file %s is readable\n", file_name);
+		ft_printf("The file %s is readable\n", file_name);
 	if (access(file_name, W_OK) == 0)
-		printf("The file %s is writable\n", file_name);
+		ft_printf("The file %s is writable\n", file_name);
 	if (access(file_name, X_OK) == 0)
-		printf("The file %s is executable\n", file_name);
+		ft_printf("The file %s is executable\n", file_name);
+	ft_printf("\n");
+}
+
+int	main(void)
+{
+	// Run 'chmod xxx script.sh && ls -la script.sh && ./pipex'
+	test_access();
+	
+	test_execve();
+	test_execve_script();
+	return (0);
 }
