caaf7f6906d52c3b1e71f3e19414537aacd9dd43
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-26 15:45:30 -0300

    Update main.c

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -5,77 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/23 11:58:26 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/26 11:50:26 by arsobrei         ###   ########.fr       */
+/*   Created: 2023/10/26 15:08:19 by arsobrei          #+#    #+#             */
+/*   Updated: 2023/10/26 15:40:18 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static int	test_execve(void)
+int	main(int argc, char **argv, char **envp)
 {
-	char	*argv[] = {"ls", "-lha", NULL};
-	char	*path;
-	pid_t	child_pid;
-
-	path = "/bin/ls";
-	
-	child_pid = fork(); // for the child process, pid == 0
-	if (child_pid == 0)
-	{
-		if (execve(path, argv, NULL) < 0)
-			ft_printf("Unable to run '%s'\n", path);
-		exit(0);
-	}
-	ft_printf("\n");
-	return (child_pid);
-}
-
-static int	test_execve_script(void)
-{
-	char	*argv[] = {"/bin/bash", "script.sh", NULL};
-	char	*path;
-	pid_t	child_pid;
-
-	path = "/bin/bash";
-
-	child_pid = fork(); // for the child process, pid == 0
-	if (child_pid == 0)
-	{
-		if (execve(path, argv, NULL) < 0)
-			ft_printf("Unable to run '%s'\n", path);
-		exit(0);
-	}
-	ft_printf("\n");
-	return (child_pid);
-}
-
-static void	test_access(void)
-{
-	const char	*file_name;
-
-	file_name = "script.sh";
-	if (access(file_name, F_OK) == 0)
-		ft_printf("The file %s exist\n", file_name);
-	if (access(file_name, R_OK) == 0)
-		ft_printf("The file %s is readable\n", file_name);
-	if (access(file_name, W_OK) == 0)
-		ft_printf("The file %s is writable\n", file_name);
-	if (access(file_name, X_OK) == 0)
-		ft_printf("The file %s is executable\n", file_name);
-}
-
-int	main(void)
-{
-	pid_t	pid;
-
-	pid = getpid();
-	ft_printf("main PID: %d\n", pid);
-	
-	// Run 'chmod xxx script.sh && ls -la script.sh && ./pipex'
-	test_access();
-
-	pid = test_execve();
-    pid = test_execve_script();
+	if (argc != 5)
+		handle_error(1);
+	handle_error(0);
+	(void)argv;
+	(void)envp;
 	return (0);
 }
