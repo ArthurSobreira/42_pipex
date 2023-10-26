7c6aaa2c371b42656bcd9577f8742af37ab42dbb
Arthur Sobreira <tutusobreira@gmail.com>
2023-10-26 15:43:52 -0300

    Update pipex header

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 12:01:30 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/25 12:02:06 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/10/26 15:38:54 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,8 @@
 # include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <sys/wait.h>
+
+void	handle_error(short exit_code);
 
 #endif
