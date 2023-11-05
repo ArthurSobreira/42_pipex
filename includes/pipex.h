539f6c04c5a8ea3a9871dfba048336f13264d968
Arthur <tutusobreira@gmail.com>
2023-11-05 16:26:35 -0300

    Update pipex header

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 12:01:30 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/10/26 15:38:54 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/05 16:04:04 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,14 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <sys/wait.h>
+
+typedef struct s_pipex
+{
+	char	*input_file;
+	char	*output_file;
+	int		fd_input;
+	int		fd_output;
+}			t_pipex;
 
 void	handle_error(short exit_code);
 
