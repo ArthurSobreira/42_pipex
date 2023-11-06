c13b45488b1b3c8f94d2e985a823c89616323865
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-06 14:51:50 -0300

    Update pipex header

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 12:01:30 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/05 16:04:04 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/06 11:56:29 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,29 @@
 
 typedef struct s_pipex
 {
-	char	*input_file;
-	char	*output_file;
-	int		fd_input;
-	int		fd_output;
+	int		fd_input_file;
+	int		fd_output_file;
+	int		fd_pipe[2];
 }			t_pipex;
 
+typedef struct s_cmd
+{
+	char	*cmd;
+	char	**argv;
+	char	**envp;
+}			t_cmd;
+
+typedef enum e_bool
+{
+	FALSE,
+	TRUE
+}		t_bool;
+
+void	start_io_files(t_pipex *pipex, char *argv[]);
+void	start_input_file(t_pipex *pipex, char *in_file);
+void	start_output_file(t_pipex *pipex, char *out_file);
+
 void	handle_error(short exit_code);
+void	handle_file_error(short exit_code, char *file_name);
 
 #endif
