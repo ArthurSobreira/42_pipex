ca58ebbf6a802b34b63f902f9a3bdae6021f85a8
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-07 19:25:10 -0300

    Update main.c

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 15:08:19 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/07 17:20:06 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/07 19:21:38 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,10 @@ void	get_commands(t_pipex *pipex)
 	index = 0;
 	while (index < pipex->commands)
 	{
-		write(0, NULL, 0);
+		// write(0, NULL, 0);
 		if (index == 0)
 		{
+			//ft_printf("entrou1\n");
 			pipex->commands_array[index].pid = -1;
 			pipex->commands_array[index].pros = INITIAL;
 			pipex->commands_array[index].cmd = "/usr/bin/cat";
@@ -33,10 +34,18 @@ void	get_commands(t_pipex *pipex)
 		}
 		else if (index == 1)
 		{
+			//ft_printf("entrou2\n");
+			char **argv = (char **)malloc(sizeof(char *) * 3);
+			if (argv != NULL)
+			{
+				argv[0] = "grep";
+				argv[1] = "teste";
+				argv[2] = NULL;
+			}
 			pipex->commands_array[index].pid = -1;
 			pipex->commands_array[index].pros = FINAL;
 			pipex->commands_array[index].cmd = "/usr/bin/grep";
-			pipex->commands_array[index].argv = (char *[]){"grep", "teste", NULL};
+			pipex->commands_array[index].argv = argv;
 			pipex->commands_array[index].envp = NULL;
 		}
 		index++;
@@ -60,7 +69,10 @@ int	main(int argc, char *argv[], char *envp[])
 		// 	{-1, FINAL, "/usr/bin/grep", ((char *[]){"grep", "teste", NULL}), NULL}
 		// };
 
+		ft_printf("argumentos: %s\n", pipex.commands_array[1].argv[1]);
+
 		execute_commands(&pipex);
+		free(pipex.commands_array[1].argv);
 		free(pipex.commands_array);
 		handle_error(0);
 		(void)envp;
