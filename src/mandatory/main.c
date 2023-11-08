daf065f82b265f7945d2776f088255a47d67a2e9
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-08 12:30:09 -0300

    Update main.c

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 15:08:19 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/08 12:22:25 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/08 12:28:31 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int argc, char *argv[], char *envp[])
 	{
 		pipex.commands = argc - 3;
 		start_io_files(&pipex, argv);
-		get_commands(&pipex, argv ,envp);
+		get_commands(&pipex, argv, envp);
 		execute_commands(&pipex);
 		clear_all(&pipex, 0);
 	}
