43ae83a2e1b0f7401722452bce154ae048da1937
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-08 15:34:52 -0300

    Update clear.c

---
+++ src/mandatory/clear.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clear.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 15:28:20 by arsobrei          #+#    #+#             */
+/*   Updated: 2023/11/08 15:28:41 by arsobrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	clear_all(t_pipex *pipex, short exit_code)
+{
+	close(pipex->fd_input_file);
+	close(pipex->fd_output_file);
+	free(pipex->commands_array);
+	handle_error(exit_code);
+}
+
+void	free_split(char **split)
+{
+	size_t	index;
+
+	index = 0;
+	while (split[index] != NULL)
+	{
+		free(split[index]);
+		index++;
+	}
+	free(split);
+}
+
