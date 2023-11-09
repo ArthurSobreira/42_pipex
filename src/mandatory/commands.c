4740fa19ba9f592dc4c8246f2dc7be7ad1bda61b
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-09 20:41:08 -0300

    Update commands.c

--- src/mandatory/commands.c
+++ src/mandatory/commands.c
@@ -6,54 +6,11 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 12:07:01 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/08 19:32:17 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/09 20:38:33 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-char	*get_cmd_path(char *cmd_name, char *envp[])
-{
-	size_t	env_index;
-	size_t	path_index;
-	char	**split;
-	char	**split_path;
-	
-	if (*cmd_name == '\0')
-		handle_error(4);
-	env_index = 0;
-	while ((envp[env_index]) && (ft_strncmp(envp[env_index], "PATH=", 5)))
-		env_index++;
-	
-	split_path = ft_split(envp[env_index] + 5, ':');
-	
-	path_index = 0;
-	while (split_path[path_index] != NULL)
-	{
-		ft_printf("%s/\n", split_path[path_index]);
-		path_index++;
-	}
-
-	split = ft_split(cmd_name, SPACE);
-	return (ft_strjoin("/usr/bin/", split[0]));
-}
-
-char	**get_arguments(char *argv)
-{
-	char	**split_args;
-	size_t	args;
-
-	split_args = ft_split(argv, SPACE);
-	args = 0;
-	while (split_args[args] != NULL)
-		args++;
-	if (args == 1)
-	{
-		free_split(split_args);
-		return (NULL);
-	}
-	return (split_args);
-}
 
 void	get_commands(t_pipex *pipex, char *argv[], char *envp[])
 {
@@ -78,4 +35,76 @@ void	get_commands(t_pipex *pipex, char *argv[], char *envp[])
 		cmd_index++;
 		index++;
 	}
+}
+
+char	*get_cmd_path(char *cmd_name, char *envp[])
+{
+	size_t	env_index;
+	char	**split_path;
+	char	*cmd;
+	char	*with_path;
+
+	cmd = get_cmd_name(cmd_name);
+	with_path = NULL;
+	env_index = 0;
+	while ((envp[env_index]) && (ft_strncmp(envp[env_index], "PATH=", 5)))
+		env_index++;
+	split_path = ft_split(envp[env_index] + 5, TWO_POINTS);
+	with_path = validate_path(cmd, split_path);
+	return (with_path);
+}
+
+char	*get_cmd_name(char *cmd_name)
+{
+	size_t	len_cmd;
+
+	len_cmd = 0;
+	while ((*cmd_name != '\0') && (*cmd_name == SPACE))
+		cmd_name++;
+	while ((cmd_name[len_cmd] != '\0') && (cmd_name[len_cmd] != SPACE))
+		len_cmd++;
+	return (ft_substr(cmd_name, 0, len_cmd));
+}
+
+char	**get_arguments(char *argv)
+{
+	char	**split_args;
+	size_t	args;
+
+	split_args = ft_split(argv, SPACE);
+	args = 0;
+	while (split_args[args] != NULL)
+		args++;
+	if (args == 1)
+	{
+		free_split(split_args);
+		return (NULL);
+	}
+	return (split_args);
+}
+
+char	*validate_path(char *cmd_name, char **split_path)
+{
+	size_t	index;
+	char	*path;
+	char	*with_path;
+
+	index = 0;
+	path = NULL;
+	with_path = NULL;
+	while (split_path[index] != NULL)
+	{
+		path = ft_strjoin(split_path[index], "/");
+		with_path = ft_strjoin(path, cmd_name);
+		free(path);
+		if (access(with_path, F_OK | X_OK) == 0)
+		{
+			free_variables(cmd_name, split_path);
+			return (with_path);
+		}
+		free(with_path);
+		index++;
+	}
+	free_variables(cmd_name, split_path);
+	return (NULL);
 }
