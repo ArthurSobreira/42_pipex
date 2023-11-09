601effbbf74aebba2a085a857831cf964c33cf0a
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-09 17:26:40 -0300

    Update error.c

--- src/mandatory/error.c
+++ src/mandatory/error.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 15:12:37 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/09 12:03:49 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/09 17:02:12 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,26 @@ void	handle_error(short exit_code)
 {
 	if (exit_code == 0)
 		ft_printf("\033[32;3mPipex executed successfully :)\033[0m\n");
+	if (exit_code == CMD_NOT_FOUND)
+		perror("\033[31;3mError, command not found :(\033[0m\n");
 	if (exit_code == 1)
-		ft_printf("\033[31;3mError, invalid parameter usage :(\033[0m\n");
+		perror("\033[31;3mError, invalid parameter usage :(\033[0m\n");
 	if (exit_code == 2)
-		ft_printf("\033[31;3mError, cannot initialize pipe :(\033[0m\n");
+		perror("\033[31;3mError, cannot initialize pipe :(\033[0m\n");
 	if (exit_code == 3)
-		ft_printf("\033[31;3mError, cannot initialize fork :(\033[0m\n");
+		perror("\033[31;3mError, cannot initialize fork :(\033[0m\n");
 	if (exit_code == 4)
-		ft_printf("\033[31;3mError, cannot initialize commands :(\033[0m\n");
-	if (exit_code == 5)
-		ft_printf("\033[31;3mError, invalid command :(\033[0m\n");
+		perror("\033[31;3mError, cannot initialize commands :(\033[0m\n");
 	exit(exit_code);
 }
 
-void	handle_file_error(short exit_code, char *file_name)
+void	handle_file_error(short exit_code)
 {
 	if (exit_code == 1)
-		ft_printf("\033[31;3mError, '%s' file not found :(\033[0m\n", file_name);
+		perror("\033[31;3mError, file not found :(\033[0m\n");
 	if (exit_code == 2)
-		ft_printf("\033[31;3mError, cannot read file '%s' :(\033[0m\n", file_name);
+		perror("\033[31;3mError, cannot read file :(\033[0m\n");
 	if (exit_code == 3)
-		ft_printf("\033[31;3mError, cannot open file '%s' :(\033[0m\n", file_name);
+		perror("\033[31;3mError, cannot open file :(\033[0m\n");
 	exit(exit_code);
 }
