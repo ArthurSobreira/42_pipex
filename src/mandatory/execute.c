fb10be68d4b1eb549000ec1883f9d2e89e4a7fff
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-10 11:31:03 -0300

    Update execute.c

--- src/mandatory/execute.c
+++ src/mandatory/execute.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 20:01:15 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/09 20:37:19 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/10 11:27:47 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,31 +39,42 @@ void	execute_commands(t_pipex *pipex)
 
 void	exec_child_process(t_pipex *pipex, t_cmd *command)
 {
+	if (command->proc_type == INITIAL)
+		initial_process(pipex, command);
+	else if (command->proc_type == FINAL)
+		final_process(pipex, command);
+}
+
+void	initial_process(t_pipex *pipex, t_cmd *command)
+{
 	short	read_pipe;
 	short	write_pipe;
 
 	read_pipe = pipex->fd_pipe[0];
 	write_pipe = pipex->fd_pipe[1];
-	if (command->proc_type == INITIAL)
-	{
-		dup2(pipex->fd_input_file, STDIN_FILENO);
-		close(read_pipe);
-		dup2(write_pipe, STDOUT_FILENO);
-		close(write_pipe);
-		if (command->cmd == NULL)
-			handle_error(CMD_NOT_FOUND);
-		else if (execve(command->cmd, command->argv, command->envp) < 0)
-			handle_error(CMD_NOT_FOUND);
-	}
-	else if (command->proc_type == FINAL)
-	{
-		dup2(pipex->fd_output_file, STDOUT_FILENO);
-		close(write_pipe);
-		dup2(read_pipe, STDIN_FILENO);
-		close(read_pipe);
-		if (command->cmd == NULL)
-			handle_error(CMD_NOT_FOUND);
-		else if (execve(command->cmd, command->argv, command->envp) < 0)
-			handle_error(CMD_NOT_FOUND);
-	}
+	dup2(pipex->fd_input_file, STDIN_FILENO);
+	close(read_pipe);
+	dup2(write_pipe, STDOUT_FILENO);
+	close(write_pipe);
+	if (command->cmd == NULL)
+		handle_error(CMD_NOT_FOUND);
+	else if (execve(command->cmd, command->argv, command->envp) < 0)
+		handle_error(CMD_NOT_FOUND);
+}
+
+void	final_process(t_pipex *pipex, t_cmd *command)
+{
+	short	read_pipe;
+	short	write_pipe;
+
+	read_pipe = pipex->fd_pipe[0];
+	write_pipe = pipex->fd_pipe[1];
+	dup2(pipex->fd_output_file, STDOUT_FILENO);
+	close(write_pipe);
+	dup2(read_pipe, STDIN_FILENO);
+	close(read_pipe);
+	if (command->cmd == NULL)
+		handle_error(CMD_NOT_FOUND);
+	else if (execve(command->cmd, command->argv, command->envp) < 0)
+		handle_error(CMD_NOT_FOUND);
 }
