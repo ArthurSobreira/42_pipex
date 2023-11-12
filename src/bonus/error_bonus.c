765e4aa9ce5dc473bdad7c2aa6fedc07fb8e034d
Arthur <tutusobreira@gmail.com>
2023-11-12 12:45:08 -0300

    Update error_bonus.c

--- src/bonus/error_bonus.c
+++ src/bonus/error_bonus.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 15:12:37 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/10 12:14:41 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/12 12:41:47 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ void	handle_error(short exit_code)
 		perror("\033[31;3mError, cannot initialize fork :(\033[0m\n");
 	if (exit_code == 4)
 		perror("\033[31;3mError, cannot initialize commands :(\033[0m\n");
+	if (exit_code == 5)
+		perror("\033[31;3mError, invalid here_doc usage :(\033[0m\n");
 	exit(exit_code);
 }
 
