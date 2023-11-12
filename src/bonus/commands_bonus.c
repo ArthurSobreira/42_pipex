9fce893697bc90d38fc64dd9070a11ff9ac45416
Arthur <tutusobreira@gmail.com>
2023-11-12 11:39:01 -0300

    Update commands_bonus.c

--- src/bonus/commands_bonus.c
+++ src/bonus/commands_bonus.c
@@ -6,7 +6,7 @@
 /*   By: arsobrei <arsobrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 12:07:01 by arsobrei          #+#    #+#             */
-/*   Updated: 2023/11/10 15:22:30 by arsobrei         ###   ########.fr       */
+/*   Updated: 2023/11/12 11:38:07 by arsobrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ void	get_commands(t_pipex *pipex, char *argv[], char *envp[])
 	{
 		pipex->commands_array[index].pid = -1;
 		pipex->commands_array[index].cmd = get_cmd_path(argv[cmd_index], envp);
-		// if (ft_strchr(pipex->commands_array[index].cmd, '/'))
-		// 	pipex->success = FALSE;
+		if (!ft_strchr(pipex->commands_array[index].cmd, '/'))
+			pipex->success = FALSE;
 		pipex->commands_array[index].argv = get_arguments(argv[cmd_index]);
 		pipex->commands_array[index].envp = envp;
 		if (index == 0)
@@ -111,6 +111,6 @@ char	*validate_path(char *cmd_name, char **split_path)
 		free(with_path);
 		index++;
 	}
-	free_variables(cmd_name, split_path);
+	free_variables(NULL, split_path);
 	return (NULL);
 }
