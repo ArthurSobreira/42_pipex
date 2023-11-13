4e9011941bd74f4146e70d1027b8a148f299bad2
Arthur Sobreira <tutusobreira@gmail.com>
2023-11-13 12:41:15 -0300

    Update here_doc_bonus.c

--- src/bonus/here_doc_bonus.c
+++ src/bonus/here_doc_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/pipex_bonus.h"
+#include "pipex_bonus.h"
 
 void	here_doc(t_pipex *pipex)
 {
@@ -20,9 +20,9 @@ void	here_doc(t_pipex *pipex)
 	line = NULL;
 	if (pipe(pipe_fd) < 0)
 		handle_error(2);
-	while (1)
+	while (TRUE)
 	{
-		ft_putstr_fd("pipex: \033[34;3mhere_doc\033[0m> ", STDOUT_FILENO);
+		ft_putstr_fd("pipex: \033[35;3mhere_doc\033[0m> ", STDOUT_FILENO);
 		line = get_next_line(STDIN_FILENO);
 		if (!ft_strncmp(line, pipex->limiter, ft_strlen(pipex->limiter)))
 		{
@@ -32,6 +32,7 @@ void	here_doc(t_pipex *pipex)
 		ft_putstr_fd(line, pipe_fd[1]);
 		free(line);
 	}
+	get_next_line(CLEAR_STATIC);
 	close(pipe_fd[1]);
 	pipex->fd_input_file = pipe_fd[0];
 }
